c55b20338d0fb34a401c8a9ec545551c409aaf12
Karthik_FP <karthik.l@freshprints.com>
2024-01-21 13:50:20 +0530

    first push from ready stock

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'high-resolution-manometry', component: Page1Component},
+  {path: 'ZepHR', component: Page2Component},
+  {path: 'hydrogen-breathe-test', component: Page3Component},
+  {path: 'catheter', component: Page4Component},
+  {path: 'contact', component: ContactUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import { FooterComponent } from './footer/footer.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Navbar2Component } from './navbar2/navbar2.component';
+import { Page4Component } from './page4/page4.component';
+import { WhatwedoComponent } from './whatwedo/whatwedo.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AddressListComponent } from './address-list/address-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Navbar2Component,
+    Page4Component,
+    WhatwedoComponent,
+    ContactUsComponent,
+    AddressListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
